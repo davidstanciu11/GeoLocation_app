1ba9515aafd154fc070550ebe6e0d20e4dde123a
davidstanciu11 <stanciudavid169@gmail.com>
2023-04-18 13:20:42 +0300

    form started

--- GeolocationApp/GeoLocation.js
+++ GeolocationApp/GeoLocation.js
@@ -1,6 +1,16 @@
-// const btnWelcome = document.getElementById("btn_welcome");
-// const welcomeSection = document.querySelector(".welcome");
-// const heroSection = document.querySelector(".hero");
+const joggingAction = document.querySelector("#jogging");
+const cyclingAction = document.querySelector("#cycling");
+const meditationAction = document.querySelector("#meditation");
+const groceriesAction = document.querySelector("#groceries");
+const distanceInpt = document.querySelector(".form_row_distance");
+const stressInpt = document.querySelector(".form_row_stress");
+const amountInpt = document.querySelector(".form_row_amount");
+const durationInpt = document.querySelector(".form_row_duration");
+const cadenceInpt = document.querySelector(".form_row_cadence");
+const elevInpt = document.querySelector(".form_row_elev");
+const typeInpt = document.querySelector(".form_row_type");
+const personInpt = document.querySelector(".form_row_person");
+
 
 const monthNames = [
 	"January",
@@ -17,30 +27,40 @@ const monthNames = [
 	"December",
 ];
 
-if(navigator.geolocation) {
-	navigator.geolocation.getCurrentPosition((position) => {
-		const {latitude} = position.coords;
-		const {longitude} = position.coords;
-		const coords = [latitude, longitude];
-		const map = L.map("map").setView(coords, 14);
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(
+		(position) => {
+			const { latitude } = position.coords;
+			const { longitude } = position.coords;
+			const coords = [latitude, longitude];
+			const map = L.map("map").setView(coords, 14);
 
-		L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-			attribution:
-				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-		}).addTo(map);
+			L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				attribution:
+					'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+			}).addTo(map);
 
-		L.marker(coords)
-			.addTo(map)
-			.bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-			.openPopup();
-	},
-	() => {
-		window.prompt(`Couldn't receive your position. Please Retry!`)
-	})
+			map.on("click", (mapEvent) => {
+				const { lat, lng } = mapEvent.latlng;
+				L.marker([lat, lng])
+					.addTo(map)
+					.bindPopup(
+						L.popup({
+							maxWidth: 250,
+							minWidth: 100,
+							autoClose: false,
+							closeOnClick: false,
+							className: "jogging_popup",
+						})
+					)
+					.setPopupContent("hello")
+					.openPopup();
+			});
+		},
+		() => {
+			window.prompt(`Couldn't receive your position. Please Retry!`);
+		}
+	);
 }
 //Events
 
-// btnWelcome.addEventListener("click", () => {
-// 	welcomeSection.classList.add("hidden");
-// 	heroSection.style.display = "flex";
-// });
