79b078b53fd8194cc5019fcff407564bd95d8d1a
davidstanciu11 <stanciudavid169@gmail.com>
2023-04-24 14:41:39 +0300

    task implemented

--- GeolocationApp/GeoLocation.js
+++ GeolocationApp/GeoLocation.js
@@ -37,20 +37,84 @@ const monthNames = [
 	"December",
 ];
 
+//Task Class
+class Task {
+	date = new Date();
+	id = (Date.now() + "").slice(-10);
+	constructor(coords, duration) {
+		this.coords = coords; //lat, lng
+		this.duration = duration;
+	}
+}
+
+class Jogging extends Task {
+	type = "jogging";
+	constructor(coords, distance, duration, cadence) {
+		super(coords, duration);
+		this.cadence = cadence;
+		this.distance = distance;
+		this.calcPace();
+	}
+
+	calcPace() {
+		this.pace = this.duration / this.distance;
+		return this.pace;
+	}
+}
+
+class Cycling extends Task {
+	type = "cycling";
+	constructor(coords, distance, duration, elevation) {
+		super(coords, duration);
+		this.elevation = elevation;
+		this.distance = distance;
+		this.calcSpeed();
+	}
+
+	calcSpeed() {
+		this.speed = this.distance / (this.duration / 60);
+		return this.speed;
+	}
+}
+
+class Meditation extends Task {
+	type = "meditation";
+	constructor(coords, stress, duration, type) {
+		super(coords, duration);
+		this.stress = stress;
+		this.type = type;
+		this.calcStress();
+	}
+
+	calcStress () {
+		this.relief = (this.stress% + this.duration) / 10;
+		return this.relief;
+	}
+}
+class Groceries extends Task {
+	type = "groceries";
+	constructor(coords, amount, duration, person) {
+		super(coords, duration);
+		this.amount = amount;
+		this.person = person;
+	}
+}
+
 //Map Class
 
 class Map {
 	#map;
 	#mapEvent;
+	#activities = [];
 	constructor() {
 		this._getPosition();
 		this._hideFields();
 		btnExport.addEventListener("click", () => {
-		form.addEventListener("submit", this._newTask.bind(this));
+			form.addEventListener("submit", this._newTask.bind(this));
 		});
 
 		activities.addEventListener("click", () => {
-		activities.addEventListener("change", this._toggleFields);
+			activities.addEventListener("change", this._toggleFields);
 		});
 	}
 
@@ -89,6 +153,18 @@ class Map {
 		elevSection.classList.add("form_row_hidden");
 		typeSection.classList.add("form_row_hidden");
 		personSection.classList.add("form_row_hidden");
+	}
+
+	_clearInputs() {
+		distanceInpt.value =
+			durationInpt.value =
+			amountInpt.value =
+			stressInpt.value =
+			cadenceInpt.value =
+			elevInpt.value =
+			typeInpt.value =
+			personInpt.value =
+				"";
 	}
 
 	_toggleFields() {
@@ -133,19 +209,69 @@ class Map {
 	}
 
 	_newTask(e) {
-		e.preventDefault();
-		distanceInpt.value =
-			durationInpt.value =
-			amountInpt.value =
-			stressInpt.value =
-			cadenceInpt.value =
-			elevInpt.value =
-			typeInpt.value =
-			personInpt.value =
-				"";
+		const validInpt = (...inpt) => {
+			return inpt.every((i) => Number.isFinite(i));
+		};
+		const allPositive = (...inpt) => inpt.every((i) => i > 0);
 
+		e.preventDefault();
+		const typeOfTask = activities.value;
+		const duration = +durationInpt.value;
 		const { lat, lng } = this.#mapEvent.latlng;
-		L.marker([lat, lng])
+		let activity;
+
+		if (typeOfTask === "jogging") {
+			const cadence = +cadenceInpt.value;
+			const distance = +distanceInpt.value;
+			if (
+				!validInpt(distance, duration, cadence) ||
+				!allPositive(distance, duration, cadence)
+			)
+				return console.log("Input not valid");
+
+			activity = new Jogging([lat, lng], distance, duration, cadence);
+		}
+
+		if (typeOfTask === "cycling") {
+			const elevGain = +elevInpt.value;
+			const distance = +distanceInpt.value;
+			if (
+				!validInpt(distance, duration, elevGain) ||
+				!allPositive(distance, duration, elevGain)
+			)
+				return console.log("Input not valid");
+
+			activity = new Cycling([lat, lng], distance, duration, elevGain);
+		}
+
+		if (typeOfTask === "meditation") {
+			const stress = +stressInpt.value;
+			const meditationType = typeInpt.value;
+			if (!validInpt(stress, duration) || !allPositive(stress, duration))
+				return console.log("Input not valid");
+
+			activity = new Meditation([lat, lng], stress, duration, meditationType);
+		}
+
+		if (typeOfTask === "groceries") {
+			const amount = +amountInpt.value;
+			const person = personInpt.value;
+			if (!validInpt(amount, duration) || !allPositive(amount, duration))
+				return console.log("Input not valid");
+			activity = new Groceries([lat, lng], amount, duration, person);
+		}
+
+		this.#activities.push(activity);
+		console.log(activity);
+		//Clear Inputs
+		this._clearInputs();
+
+		//Render Popup
+		this._renderPopUp(activity);
+	}
+
+	_renderPopUp(activity) {
+		L.marker(activity.coords)
 			.addTo(this.#map)
 			.bindPopup(
 				L.popup({
@@ -153,13 +279,12 @@ class Map {
 					minWidth: 100,
 					autoClose: false,
 					closeOnClick: false,
-					className: "groceries_popup",
+					className: `${activity.type}_popup`,
 				})
 			)
-			.setPopupContent("hello")
+			.setPopupContent("workout")
 			.openPopup();
 	}
 }
 
 const mapApp = new Map();
-
